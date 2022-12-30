246a4e713715690eb4c64f16829904dfe7c9a265
tolu2507 <109019116+tolu2507@users.noreply.github.com>
2022-12-30 14:10:43 +0100

    update todomodel

--- backend/models/todoModels.js
+++ backend/models/todoModels.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
   task: { type: "string", required: true },
-  isDone: { type: "boolean", required: true, default: false },
+  isDone: { type: "boolean", required: false, default: false },
   time_stated: { type: "string", required: true },
   time_finished: { type: "string", required: false },
   comment: { type: "string", required: false },
